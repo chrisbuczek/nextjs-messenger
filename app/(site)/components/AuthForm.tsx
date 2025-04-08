26460066d76d46aa67efaf40ee023909369088ef
kbuczek <krzysztofbuczek@live.com>
2025-04-08 16:14:20 +0200

    1. next init, install react-icons clsx, Create /(site)/page.tsx and /(site)/components/AuthForm.tsx

---
+++ app/(site)/components/AuthForm.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import { useState } from 'react';
+
+type Variant = 'LOGIN' | 'REGISTER';
+
+const AuthForm = () => {
+  const [variant, setVariant] = useState<Variant>('LOGIN');
+  const [isLoading, setIsLoading] = useState(false);
+  return <div>AuthForm!</div>;
+};
+
+export default AuthForm;
