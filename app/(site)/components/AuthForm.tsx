ab59f6971722417f94de6d0c94183a6eaec5a907
chrisbuczek <krzysztofbuczek@live.com>
2025-05-12 04:07:37 +0200

    2. Install prisma and MongoDB Atlas

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -1,19 +1,17 @@
 'use client';
 
+import Button from '@/app/components/Button';
 import Input from '@/app/components/inputs/Input';
 import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
+import AuthSocialButton from './AuthSocialButton';
+import { BsGithub, BsGoogle } from 'react-icons/bs';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
   const [variant, setVariant] = useState<Variant>('LOGIN');
   const [isPending, setIsPending] = useState(false);
-  //   const initialState: ActionResponse = {
-  //     name: null,
-  //     email: null,
-  //     password: null,
-  //   };
 
   //   this is another way of doing forms (better)
   //   const [state, action, isPending] = useActionState(submitAddress, undefined);
@@ -38,7 +36,6 @@ const AuthForm = () => {
   }, [variant]);
 
   const submitAddress = (e: React.FormEvent<HTMLFormElement>) => {
-    // this should be a server action when using useActionState
     e.preventDefault();
     setIsPending(true);
 
@@ -75,11 +72,72 @@ const AuthForm = () => {
           className="flex flex-col gap-y-6"
           onSubmit={handleSubmit(onSubmit)}
         >
-          <Input />
-          <Input />
-          <Input />
-          <button></button>
+          {variant === 'REGISTER' && (
+            <Input
+              id="name"
+              label="Name"
+              register={register}
+              errors={errors}
+              disabled={isPending}
+            />
+          )}
+          <Input
+            id="email"
+            label="Email address"
+            type="email"
+            register={register}
+            errors={errors}
+            disabled={isPending}
+          />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+            disabled={isPending}
+          />
+          <div>
+            <Button disabled={isPending} fullWidth type="submit">
+              {variant === 'LOGIN' ? 'Sign in' : 'Register'}
+            </Button>
+          </div>
         </form>
+
+        <div className="mt-6">
+          <div className="relative">
+            <div className="absolute inset-0 flex items-center">
+              <div className="w-full border-t border-gray-300" />
+            </div>
+            <div className="relative flex justify-center text-sm">
+              <span className="bg-white px-2 text-gray-500">
+                Or continue with
+              </span>
+            </div>
+          </div>
+
+          <div className="mt-6 flex gap-2">
+            <AuthSocialButton
+              icon={BsGithub}
+              onClick={() => socialAction('github')}
+            />
+            <AuthSocialButton
+              icon={BsGoogle}
+              onClick={() => socialAction('google')}
+            />
+          </div>
+
+          <div className="mt-6 flex justify-center gap-2 px-2 text-sm text-gray-500">
+            <div>
+              {variant === 'LOGIN'
+                ? 'New to messenger?'
+                : 'Already have an account?'}
+            </div>
+            <div onClick={toggleVariant} className="cursor-pointer underline">
+              {variant === 'LOGIN' ? 'Create an account' : 'Login'}
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
