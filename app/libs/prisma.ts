9130096eee0a757de36a13e04c0f78b431673acc
chrisbuczek <krzysztofbuczek@live.com>
2025-05-16 07:27:33 +0200

    4. Adding NextAuth.js

---
+++ app/libs/prisma.ts
@@ -0,0 +1,20 @@
+// import { PrismaClient } from '@prisma/client';
+
+// // https://authjs.dev/getting-started/adapters/prisma
+// declare global {
+//   var prisma: PrismaClient | undefined;
+// }
+
+// const client = globalThis.prisma || new PrismaClient();
+// if (process.env.NODE_ENV !== 'production') globalThis.prisma = client;
+
+// export default client;
+
+import { PrismaClient } from '@prisma/client';
+
+const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+const client = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = client;
+
+export default client;
