de8409865ef63abf5a3ef3487b9e6d7399eaffea
chrisbuczek <krzysztofbuczek@live.com>
2025-05-25 12:27:01 +0200

    Install react-hot-toast

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -7,6 +7,8 @@ import { useCallback, useState } from 'react';
 import { FieldValues, SubmitHandler, useForm } from 'react-hook-form';
 import AuthSocialButton from './AuthSocialButton';
 import { BsGithub, BsGoogle } from 'react-icons/bs';
+import toast from 'react-hot-toast';
+import { signIn } from 'next-auth/react';
 
 type Variant = 'LOGIN' | 'REGISTER';
 
@@ -36,27 +38,31 @@ const AuthForm = () => {
     }
   }, [variant]);
 
-  const submitAddress = (e: React.FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setIsPending(true);
-
-    if (variant === 'REGISTER') {
-      // Axios register
-    }
-    if (variant === 'LOGIN') {
-      // NextAuth SignIn
-    }
-  };
-
   const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsPending(true);
 
     if (variant === 'REGISTER') {
-      axios.post('/api/register', data);
+      axios
+        .post('/api/register', data)
+        .catch(() => toast.error('Something went wrong!'))
+        .finally(() => setIsPending(false));
     }
 
     if (variant === 'LOGIN') {
-      // NextAuth SignIn
+      signIn('credentials', {
+        ...data,
+        redirect: false,
+      })
+        .then((callback) => {
+          if (callback?.error) {
+            toast.error('Invalid credentials');
+          }
+
+          if (callback?.ok && callback?.error) {
+            toast.success('Logged in');
+          }
+        })
+        .finally(() => setIsPending(false));
     }
   };
 
